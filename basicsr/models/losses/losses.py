fa32f3618ce7b0e7e4f91b7669539025a8aa7579
Alexandru Brateanu <alexandru.brateanu@student.manchester.ac.uk>
2026-04-13 09:13:43 +0300

    loss adjustments

--- basicsr/models/losses/losses.py
+++ basicsr/models/losses/losses.py
@@ -26,14 +26,26 @@ def mse_loss(pred, target):
 
 class VGGPerceptualLoss(nn.Module):
     def __init__(self, device):
-        super(VGGPerceptualLoss, self).__init__()
-        vgg = models.vgg19(weights=True).features[:16]  # Until block3_conv3
+        super().__init__()
+        vgg = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).features[:16]
         self.loss_model = vgg.to(device).eval()
-        for param in self.loss_model.parameters():
-            param.requires_grad = False
+        for p in self.loss_model.parameters():
+            p.requires_grad = False
+
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+
+    def normalize(self, x):
+        return (x - self.mean) / self.std
 
     def forward(self, y_true, y_pred):
-        y_true, y_pred = y_true.to(next(self.loss_model.parameters()).device), y_pred.to(next(self.loss_model.parameters()).device)
+        device = next(self.loss_model.parameters()).device
+        y_true = y_true.to(device)
+        y_pred = y_pred.to(device)
+
+        y_true = self.normalize(y_true)
+        y_pred = self.normalize(y_pred)
+
         return F.mse_loss(self.loss_model(y_true), self.loss_model(y_pred))
 
 def multiscale_ssim_loss(y_true, y_pred, max_val=1.0):
@@ -51,7 +63,7 @@ class HybridLoss(nn.Module):
             print('not using MSE Loss')
         
         if w_perc != 0:
-            self.perceptual_loss_model = None
+            self.perceptual_loss_model = VGGPerceptualLoss
         else:
             print('not using Perc Loss')
 
