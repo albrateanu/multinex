8cc6f30cf8330e1403a1aae43857be62f44f30f3
Alexandru Brateanu <81636703+albrateanu@users.noreply.github.com>
2026-04-14 20:38:34 +0100

    loss adjust

--- basicsr/models/losses/losses.py
+++ basicsr/models/losses/losses.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn as nn
 from torch.nn import functional as F
 import numpy as np
-from pytorch_msssim import ms_ssim, ssim
+from pytorch_msssim import ssim
 import torchvision.models as models
 
 from basicsr.models.losses.loss_util import weighted_loss
@@ -40,9 +40,6 @@ class VGGPerceptualLoss(nn.Module):
         return F.mse_loss(self.loss_model(y_true), self.loss_model(y_pred))
 
 def multiscale_ssim_loss(y_true, y_pred, max_val=1.0):
-    return 1.0 - ms_ssim(y_true, y_pred, data_range=max_val, size_average=True)
-
-def ssim_loss(y_true, y_pred, max_val=1.0):
     return 1.0 - ssim(y_true, y_pred, data_range=max_val, size_average=True)
 
 class HybridLoss(nn.Module):
